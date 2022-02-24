331a27863ac0a0ceeff85159b62288a5816cb1ec
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-24 02:39:22 +0000

    wip

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,11 @@ export default function Home() {
       </Head>
 
       <header className="header">
-        <div style={{ display: "flex", gap: "12px", alignItems: "baseline" }}>
+        <div className="first-col">
           <p>test.near</p>
-          <p style={{ color: "green", fontSize: "12px" }}>10.2 Ⓝ</p>
+          <p className="available-near">10.2 Ⓝ</p>
         </div>
-        <p style={{ color: "red", cursor: "pointer" }}>Sign out</p>
+        <p className="sign-out">Sign out</p>
       </header>
 
       <div className="main">
@@ -37,13 +37,7 @@ export default function Home() {
             Connect Wallet
           </a> */}
 
-          <div
-            style={{
-              display: "flex",
-              marginTop: "36px",
-              gap: "16px",
-            }}
-          >
+          <div className="social-icons">
             <a
               href="https://twitter.com/DegenLizards"
               target={"_blank"}
@@ -73,16 +67,6 @@ export default function Home() {
             </a>
           </div>
         </main>
-
-        {/* <footer className="footer">
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by DEGEN Lizards
-          </a>
-        </footer> */}
       </div>
     </div>
   );
