ff5611467c46152ad4d726e5401ca28ce3f1df8b
Rui Santiago <ruisantiagomr@gmail.com>
2022-02-24 00:55:30 +0000

    wip

--- components/Coin.jsx
+++ components/Coin.jsx
@@ -15,22 +15,6 @@ const Coin = () => {
 
   return (
     <div className="App">
-      <div style={{ display: "flex" }}>
-        <input
-          type="radio"
-          name="site_name"
-          value="HEADS"
-          checked={selected === "heads"}
-          onChange={() => setSelected("heads")}
-        />
-        <input
-          type="radio"
-          name="site_name"
-          value="TAILS"
-          checked={selected === "tails"}
-          onChange={() => setSelected("tails")}
-        />
-      </div>
       <div id="coin" className={tossResult} key={+new Date()}>
         <div className="side-a">
           <h2>T</h2>
@@ -39,10 +23,48 @@ const Coin = () => {
           <h2>H</h2>
         </div>
       </div>
-      <h1>Flip a coin</h1>
-      <button id="btn" onClick={coinToss}>
+
+      <div className="chip-group" role="radiogroup">
+        <div
+          className={`chip ${
+            selected === "heads" ? "active" : ""
+          } chip-checkbox`}
+          aria-labelledby="radioOneLabel"
+          role="radio"
+          aria-checked="false"
+          onClick={() => setSelected("heads")}
+        >
+          <input type="radio" name="radioEx" />
+          <span id="radioOneLabel">Heads</span>
+        </div>
+        <div
+          className={`chip ${
+            selected === "tails" ? "active" : ""
+          } chip-checkbox`}
+          aria-labelledby="radioTwoLabel"
+          role="radio"
+          aria-checked="false"
+          onClick={() => setSelected("tails")}
+        >
+          <input type="radio" name="radioEx" />
+          <span id="radioTwoLabel">Tails</span>
+        </div>
+      </div>
+
+      <p>Select the Ⓝ amount to flip</p>
+      <div style={{ display: "flex", gap: "12px" }}>
+        <div className="near-amount">1 Ⓝ</div>
+        <div className="near-amount">2 Ⓝ</div>
+        <div className="near-amount">3 Ⓝ</div>
+        <div className="near-amount">4 Ⓝ</div>
+        <div className="near-amount">5 Ⓝ</div>
+      </div>
+      <a href="something" className="button3" style={{ marginTop: "16px" }}>
+        Flip
+      </a>
+      {/* <button id="btn" onClick={coinToss}>
         Coin Toss
-      </button>
+      </button> */}
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,48 @@
 import Head from "next/head";
 import Image from "next/image";
 import Coin from "../components/Coin";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>DEGEN Lizards</title>
         <meta name="description" content="DEGEN Lizards coin flip" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <header className={styles.header}>
-        <p>JOAO</p>
+      <header className="header">
+        <p>test.near</p>
+        <p>Sign out</p>
       </header>
 
-      <main className={styles.main}>
-        <h3>Welcome to DEGEN Lizards Coin Flip!</h3>
+      <div className="main">
+        <main className="body">
+          <h3>Welcome to DEGEN Lizards Coin Flip!</h3>
 
-        <Coin />
+          <Coin />
 
-        <button>Connect Wallet</button>
-      </main>
+          {/* <a href="something" className="button3">
+            Connect Wallet
+          </a> */}
+        </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <footer className="footer">
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by DEGEN Lizards
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
