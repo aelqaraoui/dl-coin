61448f7f24c3314530a2bc3aa1f9343b1024c402
Rui Santiago <ruisantiagomr@gmail.com>
2022-03-30 22:58:39 +0100

    :sparkles: add new bet array

--- src/components/CoinContainer/CoinContainer.jsx
+++ src/components/CoinContainer/CoinContainer.jsx
@@ -6,22 +6,33 @@ import coinAnimationY from "../../lotties/coinflipY.json";
 import lostAnimation from "../../lotties/Loss.json";
 import winAnimation from "../../lotties/win2.json";
 import { login } from "../../utils";
+import { toNear } from "../../utils/nearValues";
 import PlayButton from "../Core/buttons/PlayButton";
 import "./coinContainer.css";
 import lizard from "./lizard.png";
 import near from "./near.png";
 
+const bets = [
+  0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6,
+  1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3, 3.1, 3.2,
+  3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8,
+  4.9, 5, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6, 6.1, 6.2, 6.3, 6.4,
+  6.5, 6.6, 6.7, 6.8, 6.9, 7, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8,
+  8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6,
+  9.7, 9.8, 9.9, 10,
+];
+
 const CoinContainer = ({ flipStatus }) => {
   const [selectedSide, setSelectedSide] = useState("heads");
-  const [selectedAmount, setSelectedAmount] = useState(1);
+  const [selectedAmount, setSelectedAmount] = useState(0.1);
   const [showLoading, setShowLoading] = useState(false);
-  const [betsArray, setBetsArray] = useState([1, 2, 3, 4, 5]);
+  const [betsArray, setBetsArray] = useState(bets);
 
   useEffect(async () => {
     const maxBet = await window.contract.get_max_bet();
     const maxBetFinal = Math.floor(parseFloat(maxBet) / 1e24);
 
-    if (maxBetFinal > 5) {
+    if (maxBetFinal > 10) {
       let aux = [];
       for (let i = 6; i <= maxBetFinal; i++) {
         aux.push(i);
@@ -169,9 +180,7 @@ const CoinContainer = ({ flipStatus }) => {
                     window.contract.play(
                       {},
                       100000000000000,
-                      new BN("1035000000000000000000000", 10).mul(
-                        new BN(selectedAmount.toString(), 10)
-                      )
+                      new BN(toNear((selectedAmount * 1.035).toString()))
                     );
                   } catch (e) {
                     alert(
